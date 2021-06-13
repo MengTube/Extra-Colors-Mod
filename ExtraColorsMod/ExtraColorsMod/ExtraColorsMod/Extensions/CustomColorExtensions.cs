0b02599ef2b2edb6a69da6d7112b8ccd4cd9b03b
MengTube <81153785+MengTube@users.noreply.github.com>
2021-06-13 16:54:06 +0800

    Source Code For v1.0.0

---
+++ ExtraColorsMod/ExtraColorsMod/ExtraColorsMod/Extensions/CustomColorExtensions.cs
@@ -0,0 +1,19 @@
+﻿using ExtraColorsMod.Patches;
+using ExtraColorsMod.Types;
+using Reactor;
+
+namespace ExtraColorsMod.Extensions
+{
+    public static class CustomColorExtensions
+    {
+        public static bool TryGetCustomColorById(int colorId, out CustomColor customColor)
+        {
+            var originalPaletteLength = ColorSelectionPatches.OriginalPaletteLength;
+
+            var isCustomColor = colorId >= originalPaletteLength;
+            customColor = isCustomColor ? ColorSelectionPatches.CustomColors[colorId - originalPaletteLength] : null;
+
+            return isCustomColor;
+        }
+    }
+}
