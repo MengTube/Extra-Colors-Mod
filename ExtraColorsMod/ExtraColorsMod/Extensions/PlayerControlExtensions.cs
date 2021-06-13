3fe143073b5162291f47d64c7dcd50914575e3a8
MengTube <81153785+MengTube@users.noreply.github.com>
2021-06-13 16:50:31 +0800

    Add files via upload

---
+++ ExtraColorsMod/ExtraColorsMod/Extensions/PlayerControlExtensions.cs
@@ -0,0 +1,37 @@
+﻿using ExtraColorsMod.MonoBehaviours;
+using ExtraColorsMod.Types;
+using Reactor;
+using Reactor.Extensions;
+using UnityEngine;
+
+namespace ExtraColorsMod.Extensions
+{
+    public static class PlayerControlExtensions
+    {
+        public static void SetCustomColor(this PlayerControl player, Color frontColor, Color backColor, string colorName, string shortColorName)
+        {
+            Logger<ExtraColorsModPlugin>.Info(
+                $"Setting player color for '{player.Data.PlayerName}': " +
+                $"front color = {frontColor.ToString()}, back color = {backColor.ToString()}, " +
+                $"name = {colorName}, short name = {shortColorName}");
+
+            player.myRend.gameObject.ClearCyclicColor();
+            player.HatRenderer.FrontLayer.gameObject.ClearCyclicColor();
+            player.HatRenderer.BackLayer.gameObject.ClearCyclicColor();
+            if (player.CurrentPet) player.CurrentPet.gameObject.ClearCyclicColor();
+
+            var colorIndex = Palette.PlayerColors.IndexOf(frontColor);
+            if (colorIndex == -1)
+            {
+                ExtraColorsMod.AddColor(new NormalColor(frontColor, backColor, colorName, shortColorName, true));
+            }
+            
+            player.SetColor(colorIndex);
+        }
+        
+        public static bool TryGetCustomColor(this PlayerControl playerControl, out CustomColor customColor)
+        {
+            return CustomColorExtensions.TryGetCustomColorById(playerControl.Data.ColorId, out customColor);
+        }
+    }
+}
