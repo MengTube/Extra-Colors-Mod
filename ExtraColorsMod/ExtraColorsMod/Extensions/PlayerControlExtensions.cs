e430e6a3206c7570708ee90e83f16d4ad75d0130
MengTube <81153785+MengTube@users.noreply.github.com>
2021-07-08 20:31:10 +0800

    Source Code For Extra Colors Mod

---
+++ ExtraColorsMod/ExtraColorsMod/Extensions/PlayerControlExtensions.cs
@@ -0,0 +1,35 @@
+﻿using ExtraColorsMod.Types;
+using Reactor;
+using UnityEngine;
+
+namespace ExtraColorsMod.Extensions
+{
+    public static class PlayerControlExtensions
+    {
+        public static void SetCustomColor(this PlayerControl player, Color frontColor, Color backColor, string colorName)
+        {
+            Logger<ExtraColorsModPlugin>.Info(
+                $"Setting player color for '{player.Data.PlayerName}': " +
+                $"front color = {frontColor.ToString()}, back color = {backColor.ToString()}, " +
+                $"name = {colorName}");
+
+            player.myRend.gameObject.ClearCyclicColor();
+            player.HatRenderer.FrontLayer.gameObject.ClearCyclicColor();
+            player.HatRenderer.BackLayer.gameObject.ClearCyclicColor();
+            if (player.CurrentPet) player.CurrentPet.gameObject.ClearCyclicColor();
+
+            var colorIndex = Palette.PlayerColors.IndexOf(frontColor);
+            if (colorIndex == -1)
+            {
+                ExtraColorsMod.AddColor(new NormalColor(frontColor, backColor, colorName, true));
+            }
+
+            player.SetColor(colorIndex);
+        }
+
+        public static bool TryGetCustomColor(this PlayerControl playerControl, out CustomColor customColor)
+        {
+            return CustomColorExtensions.TryGetCustomColorById(playerControl.Data.ColorId, out customColor);
+        }
+    }
+}
